8e48b7846a38f10d5ae3c92c7393c78efaf503e8
djcunniffe <32771602+djcunniffe@users.noreply.github.com>
2021-07-03 11:24:57 +0200

    add discord token

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
+import os
 import discord
 from discord.ext import commands
 
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+
 client = commands.Bot(command_prefix= '/')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+client.run(DISCORD_TOKEN)
