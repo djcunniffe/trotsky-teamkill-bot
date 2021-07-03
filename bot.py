04cf8a1143ac9db00db25aa065e1a930394bcfc9
djcunniffe <32771602+djcunniffe@users.noreply.github.com>
2021-07-03 15:05:02 +0200

    store teamkill users

--- bot.py
+++ bot.py
@@ -1,18 +1,33 @@
 import os
+import uuid
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
-client = commands.Bot(command_prefix= '!')
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
 
-@client.event
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("Trotsky Development").sheet1
+
+bot = commands.Bot(command_prefix= '!')
+
+@bot.event
 async def on_ready():
     print('Bot is ready.')
 
-@client.command()
+@bot.command()
 async def teamkill(ctx, teamkiller: discord.Member, teamkilled: discord.Member):
     await ctx.send(f'{teamkiller.mention} teamkilled {teamkilled.mention}')
+    row = [str(uuid.uuid4),teamkiller.id,teamkilled.id]
+    sheet.insert_row(row,2)
 
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
