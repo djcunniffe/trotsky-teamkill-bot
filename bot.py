77f436dd4e2450fa66b0e13999a9cf130d91a51a
djcunniffe <32771602+djcunniffe@users.noreply.github.com>
2021-07-03 13:48:11 +0200

    add basic teamkill command

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 import os
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
-client = commands.Bot(command_prefix= '/')
+client = commands.Bot(command_prefix= '!')
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
+
+@client.command()
+async def teamkill(ctx, teamkiller: discord.Member, teamkilled: discord.Member):
+    await ctx.send(f'{teamkiller.mention} teamkilled {teamkilled.mention}')
 
 client.run(DISCORD_TOKEN)
